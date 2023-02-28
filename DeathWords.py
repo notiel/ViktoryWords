ed78780c659e98ad1fa9e6d77492c747635d1c5e
notiel <agereth@gmail.com>
2023-03-01 02:34:54 +0300

    dolina

--- DeathWords.py
+++ DeathWords.py
@@ -7,35 +7,34 @@ import httplib2
 import googleapiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 from typing import List
-from random import randint
+from random import randint, sample
+
+titles = 'abcdefghijklmnopqrstuvwxyz'
 
 CREDENTIALS_FILE = 'token.json'
 
-weak = 'алый белый лазурный пурпурный черный золотой серебряный изумрудный'
+weak = "ярость гнев удушье кровь"
+middle = "пламя вихрь гром дождь тьма"
+strong = "камень бамбук поток гора дракон река"
 
-special = 'вихрь тигр луч рассвет закат ледяной река ярость'
+all_techniques = ['кровь ярость', "ярость гнев", "удушье гнев", "кровь дождь", "удушье вихрь",
+                  "тема пламя", "гром тьма", "пламя вихрь", "гром гора", "поток дождь",
+                  "река дракон", "гора поток", "бамбук река", "камень бамбук", "камень дракон"]
 
-extra = 'сила пламя феникс'
+first_thresh = 5
+second_thresh = 10
 
-general = "журавль дракон цилинь черепаха танец порыв долг путь гора камень ветер вода небо тишина воздух сон шепот "
 
+strength_dict = {1: weak, 2: middle, 3: strong}
 
-base = {'Гу': 'вихрь', 'Шань': "закат", "Хо": 'ледяной', 'РиО': "река", 'Хэ': 'ярость'}
-
-east_sun = 'тигр в лучах рассвета'
-
-titles = "abcdefghijklmnoprst"
 
 # noinspection PyPep8Naming
 @dataclass
 class warrior:
     name: str
     wounds: int
-    tech_num_weak: int
+    strength: int
     tech_num: int
-    tech_num_strong: int
-    word_num: int
-    base_tech: str
     death_words: List[str] = field(default_factory=list)
     technique: List[str] = field(default_factory=list)
 
@@ -51,6 +50,7 @@ class result:
     tired2: int = 0
     wounded1: int = 0
     wounded2: int = 0
+    even: int = 0
 
 
 def error_message(text: str):
@@ -69,9 +69,9 @@ def error_message(text: str):
     error.exec_()
 
 
-def get_figth_result(warrior1: warrior, warrior2: warrior, fight_result: result):
+def get_fight_result(warrior1: warrior, warrior2: warrior, fight_result: result):
     """
-    update result of figths with one more figth data
+    update result of fights with one more fight data
     :param fight_result: current fight result structure
     :param warrior1: first warrior data
     :param warrior2: second warrior data
@@ -81,7 +81,7 @@ def get_figth_result(warrior1: warrior, warrior2: warrior, fight_result: result)
     hit2 = warrior2.wounds
     technique1 = warrior1.technique.copy()
     technique2 = warrior2.technique.copy()
-    while hit1 > 0 and hit2 > 0:
+    while technique1 or technique2:
         # first warrior
         if technique1:
             current_technique = technique1.pop(randint(0, len(technique1) - 1))
@@ -93,11 +93,6 @@ def get_figth_result(warrior1: warrior, warrior2: warrior, fight_result: result)
                         fight_result.death1 += 1
                         fight_result.win1 += 1
                         return
-
-        else:
-            fight_result.tired2 += 1
-            fight_result.win2 += 1
-            return
         # second warriors
         if technique2:
             current_technique = technique2.pop(randint(0, len(technique2) - 1))
@@ -109,11 +104,16 @@ def get_figth_result(warrior1: warrior, warrior2: warrior, fight_result: result)
                         fight_result.death2 += 1
                         fight_result.win2 += 1
                         return
-
-        else:
-            fight_result.tired1 += 1
-            fight_result.win1 += 1
-            return
+    # if (warrior1.wounds - hit1) > (warrior2.wounds - hit2):
+    '''if hit1 > hit2:
+        fight_result.tired1 += 1
+        fight_result.win2 += 1
+    # elif (warrior1.wounds - hit1) < (warrior2.wounds - hit2):
+    elif hit1 < hit2:
+        fight_result.tired2 += 1
+        fight_result.win1 += 1
+    else:'''
+    fight_result.even += 1
 
 
 class DeathWords(QtWidgets.QMainWindow, design.Ui_MainWindow):
@@ -170,78 +170,33 @@ class DeathWords(QtWidgets.QMainWindow, design.Ui_MainWindow):
         row = 1
         for character in warrior_list:
             row += 1
-            if len(character) >= 10:
+            if len(character) >= 3:
                 new_warrior = warrior(name=character[0], wounds=int(character[1]),
-                                      word_num=int(character[2]), tech_num_weak=int(character[4]),
-                                      tech_num=int(character[5]), tech_num_strong=int(character[6]),
-                                      base_tech=character[9], death_words=list(), technique=list())
-                # смерть-слова из общих и специальных для стран и школы
-                source = general + special
-                new_warrior.death_words.append(source.split()[randint(0, len(source.split())) - 1])
-                for i in range(new_warrior.word_num - 1):
+                                      strength=int(character[2]), tech_num=int(character[7]),
+                                      death_words=list(), technique=list())
+                # смерть-слова у всех по каждому
+                new_warrior.death_words.append(weak.split()[randint(0, len(weak.split())) - 1])
+                new_warrior.death_words.append(middle.split()[randint(0, len(middle.split())) - 1])
+                new_warrior.death_words.append(strong.split()[randint(0, len(strong.split())) - 1])
+                # доп слово своей силы
+                source = strength_dict[new_warrior.strength]
+                new_word = source.split()[randint(0, len(source.split())) - 1]
+                while new_word in new_warrior.death_words:
                     new_word = source.split()[randint(0, len(source.split())) - 1]
-                    while new_word in new_warrior.death_words:
-                        new_word = source.split()[randint(0, len(source.split())) - 1]
-                    new_warrior.death_words.append(new_word)
-
-                # смерть-слово для всеобщего приема
-                i = randint(0, 2)
-                new_warrior.death_words.append(extra.split()[i])
+                new_warrior.death_words.append(new_word)
 
                 # раздача приемов
-                already_used = list()
-                max_words = len(general.split()) - 1
-
-                # особый прием, если он есть
-                if new_warrior.base_tech in base.keys():
-                    second_word = general.split()[randint(0, max_words)]
-                    first_tech = base[new_warrior.base_tech] + ' ' + second_word
-                    new_warrior.technique.append(first_tech)
-                    already_used.append(second_word)
-                    new_warrior.tech_num -= 1
-                if new_warrior.base_tech == 'солнце' or new_warrior.name == 'Ху Лэй':
-                    new_warrior.technique.append(east_sun)
-                    new_warrior.tech_num_strong = - 1
-
-                # слабые приемы
-                for i in range(new_warrior.tech_num_weak):
-                    new1 = weak.split()[randint(0, len(weak.split())-1)]
-                    while new1 in already_used:
-                        new1 = weak.split()[randint(0, len(weak.split())-1)]
-                    already_used.append(new1)
-                    new2 = general.split()[randint(0, max_words)]
-                    while new2 in already_used:
-                        new1 = general.split()[randint(0, max_words)]
-                    already_used.append(new2)
-                    new_warrior.technique.append(new1 + ' ' + new2)
-
-                # обычные приемы
-                for i in range(new_warrior.tech_num):
-                    new1 = general.split()[randint(0, max_words)]
-                    while new1 in already_used:
-                        new1 = general.split()[randint(0, max_words)]
-                    already_used.append(new1)
-                    new2 = general.split()[randint(0, max_words)]
-                    while new2 in already_used:
-                        new2 = general.split()[randint(0, max_words)]
-                    new_warrior.technique.append(new1 + ' ' + new2)
-                    already_used.append(new2)
-
-                # сильные приемы
-                for i in range(new_warrior.tech_num_strong):
-                    new1 = general.split()[randint(0, max_words)]
-                    while new1 in already_used:
-                        new1 = general.split()[randint(0, max_words)]
-                    already_used.append(new1)
-                    new2 = general.split()[randint(0, max_words)]
-                    while new2 in already_used:
-                        new2 = general.split()[randint(0, max_words)]
-                    already_used.append(new2)
-                    new3 = general.split()[randint(0, max_words)]
-                    while new3 in already_used:
-                        new3 = general.split()[randint(0, max_words)]
-                    already_used.append(new3)
-                    new_warrior.technique.append(new1 + ' ' + new2 + ' ' + new3)
+                weak_tech_num = first_thresh if new_warrior.tech_num >= first_thresh else new_warrior.tech_num
+                middle_tech_num = second_thresh - first_thresh if new_warrior.tech_num >= second_thresh \
+                    else max(new_warrior.tech_num - first_thresh, 0)
+                strong_tech_num = max(new_warrior.tech_num - second_thresh, 0)
+                indices = sample(range(0, first_thresh), weak_tech_num)
+                middle_indices = sample(range(first_thresh, second_thresh), middle_tech_num)
+                strong_indices = sample(range(second_thresh, len(all_techniques)), strong_tech_num)
+                indices.extend(middle_indices)
+                indices.extend(strong_indices)
+                for ind in indices:
+                    new_warrior.technique.append(all_techniques[ind])
                 self.warrior_data.append(new_warrior)
                 self.CBWar1.addItem(character[0])
                 self.CBWar2.addItem(character[0])
@@ -251,10 +206,9 @@ class DeathWords(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 for technique in new_warrior.technique:
                     if technique.lower() not in techniques:
                         techniques.append(technique.lower())
-                warrior_result = [' '.join(new_warrior.death_words)]
-                warrior_result.extend(new_warrior.technique)
+                warrior_result = [' '.join(new_warrior.death_words), ', '.join(new_warrior.technique)]
                 request_body = {"valueInputOption": "RAW",
-                                "data": [{"range": 'k%i:%s%i' % (row, titles[len(warrior_result)+10], row),
+                                "data": [{"range": 'i%i:j%i' % (row, row),
                                           "values": [warrior_result]}]}
                 request = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.spreadsheet_id,
                                                                            body=request_body)
@@ -274,7 +228,7 @@ class DeathWords(QtWidgets.QMainWindow, design.Ui_MainWindow):
         warrior2 = [w for w in self.warrior_data if w.name == warrior2_name][0]
         new_result = result()
         for i in range(self.SpinNum.value()):
-            get_figth_result(warrior1, warrior2, new_result)
+            get_fight_result(warrior1, warrior2, new_result)
         self.LblWar1Vik.setText("%s победил %i раз" % (warrior1_name, new_result.win1))
         self.LblWar2Vik.setText("%s победил %i раз" % (warrior2_name, new_result.win2))
         self.LblWar1Wounds.setText("Из них ранения: %i" % new_result.death1)
@@ -290,15 +244,94 @@ class DeathWords(QtWidgets.QMainWindow, design.Ui_MainWindow):
                         f.write(warrior1.name + " vs " + warrior2.name + '\n')
                         new_result = result()
                         for i in range(self.SpinNum.value()):
-                            get_figth_result(warrior1, warrior2, new_result)
-                        f.write("%s победил %i раз, Из них ранения: %i, Вымотал: %i раз, "
+                            get_fight_result(warrior1, warrior2, new_result)
+                        f.write("%s победил %i раз, Из них смерть: %i, Вымотал: %i раз, "
                                 "получил %i ран\n" % (warrior1.name, new_result.win1, new_result.death1,
                                                       new_result.tired1, new_result.wounded1))
-                        f.write("%s победил %i раз, Из них ранения: %i, Вымотал: %i раз, "
+                        f.write("%s победил %i раз, Из них смерть: %i, Вымотал: %i раз, "
                                 "получил %i ран\n" % (warrior2.name, new_result.win2, new_result.death2,
                                                       new_result.tired2, new_result.wounded2))
-        f.close()
-        self.statusbar.showMessage("Результат записан в файл fights.txt")
+                        f.write(f"Ничья {new_result.even} раз\n")
+        '''
+        with open("fights1.txt", "w") as f:
+            for i in range(self.SpinNum.value()):
+                f.write(f"{i}-ая волна сражений\n")
+                for warrior1 in self.warrior_data:
+                    warrior2 = self.warrior_data[randint(0, len(self.warrior_data) - 1)]
+                    while warrior1.name == warrior2.name:
+                        warrior2 = self.warrior_data[randint(0, len(self.warrior_data) - 1)]
+                    f.write(warrior1.name + " vs " + warrior2.name + '\n')
+                    new_result = result()
+                    get_fight_result(warrior1, warrior2, new_result)
+                    print(new_result)
+                    if new_result.win1 > 0:
+                        s_win = f"Победил {warrior1.name}. "
+                        self.get_new_technique(warrior1)
+                        warrior1.tech_num += 1
+                        s_win += f"Получил технику {warrior1.technique[-1]}, у него {warrior1.tech_num} ударов\n"
+                    elif new_result.win2 > 0:
+                        s_win = f"Победил {warrior2.name}. "
+                        self.get_new_technique(warrior2)
+                        warrior2.tech_num += 1
+                        s_win += f"Получил технику {warrior2.technique[-1]}, у него {warrior2.tech_num} ударов \n"
+                    else:
+                        s_win = "Ничья. "
+                    if new_result.death1 > 0 or new_result.death2 > 0:
+                        s_reason = "Причина: снял все ци. "
+                    else:
+                        s_reason = "Причина: Кончились удары. "
+                    f.write(s_win + s_reason + f'Снято ци: {new_result.wounded1}, {new_result.wounded2}\n')
+                    '''
+        with open("fights2.txt", "w") as f:
+            for i in range(self.SpinNum.value()):
+                f.write(f"{i}-ая волна сражений. С добавлением ци\n")
+                for warrior1 in self.warrior_data:
+                    warrior2 = self.warrior_data[randint(0, len(self.warrior_data) - 1)]
+                    while warrior1.name == warrior2.name:
+                        warrior2 = self.warrior_data[randint(0, len(self.warrior_data) - 1)]
+                    f.write(warrior1.name + " vs " + warrior2.name + '\n')
+                    new_result = result()
+                    get_fight_result(warrior1, warrior2, new_result)
+                    print(new_result)
+                    if new_result.win1 > 0:
+                        s_win = f"Победил {warrior1.name}. "
+                        self.get_new_technique(warrior1)
+                        warrior1.tech_num += 1
+                        warrior1.wounds += 1
+                        s_win += f"Получил технику {warrior1.technique[-1]}, у него {warrior1.tech_num} ударов " \
+                                 f"и {warrior1.wounds} ци\n"
+                    elif new_result.win2 > 0:
+                        s_win = f"Победил {warrior2.name}. "
+                        self.get_new_technique(warrior2)
+                        warrior2.tech_num += 1
+                        warrior2.wounds += 1
+                        s_win += f"Получил технику {warrior2.technique[-1]}, у него {warrior2.tech_num} ударов " \
+                                 f"и {warrior2.wounds} ци\n"
+                    else:
+                        s_win = "Ничья. "
+                    if new_result.death1 > 0 or new_result.death2 > 0:
+                        s_reason = "Причина: снял все ци. "
+                    else:
+                        s_reason = "Причина: Кончились удары. "
+                    f.write(s_win + s_reason + f'Снято ци: {new_result.wounded1}, {new_result.wounded2}\n')
+            self.statusbar.showMessage("Результат записан в файл fights.txt, fight1.txt, fight2.txt")
+
+    @staticmethod
+    def get_new_technique(character: warrior):
+        if character.tech_num < len(all_techniques):
+            if character.tech_num < first_thresh:
+                ind_start = 0
+                ind_end = first_thresh
+            elif character.tech_num < second_thresh:
+                ind_start = first_thresh
+                ind_end = second_thresh
+            else:
+                ind_start = second_thresh
+                ind_end = len(all_techniques)
+            new_technique = all_techniques[randint(ind_start, ind_end-1)]
+            while new_technique in character.technique:
+                new_technique = all_techniques[randint(ind_start, ind_end-1)]
+            character.technique.append(new_technique)
 
 
 def initiate_exception_logging():
